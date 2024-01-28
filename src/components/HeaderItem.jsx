9769b6d567b932d2061bbc0e02de00979fcdf39b
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-01-28 02:46:54 +0100

    Add logo and header components

---
+++ src/components/HeaderItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HeaderItem({Icon, name}) {
+  return (
+    <div className='flex items-center gap-3 text-[15px] font semibold cursor-pointer hover:underline
+                    underline-offset-8 mb-2'>
+        <Icon />
+        <h2 className=''>{name}</h2>
+    </div>
+  )
+}
+
+export default HeaderItem
---
+++ src/components/header.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import logo from '../assets/logo.png';
+import { useState } from 'react';
+import {HiStar, HiMagnifyingGlass,
+        HiHome, HiPlayCircle, HiTv } from 'react-icons/hi2';
+import { HiPlus, HiDotsVertical } from 'react-icons/hi';
+import HeaderItem from './headerItem';
+function Header() {
+    const [toggle,setToggle] = useState(false);
+    const menu = [
+        {name : 'HOME', icon: HiHome},
+        {name : 'SEARCH', icon: HiMagnifyingGlass},
+        {name : 'WATCHLIST', icon: HiPlus},
+        {name : 'ORIGINALS', icon: HiStar},
+        {name : 'MOVIES', icon: HiPlayCircle},
+        {name : 'SERIES', icon: HiTv},
+
+    ]
+  return (
+    <div className='flex items-center justify-between p-5'>
+        <div className='flex items-center gap-8'>
+        <img src={logo} className='w-[80px] md:w-[115px] object-cover' />
+            <div className='hidden md:flex gap-8'>
+                {menu.map((item, key) => (
+                    <HeaderItem Icon={item.icon} name={item.name}/>
+                
+                ))}
+            </div>
+            <div className='flex md:hidden gap-5'>
+                {menu.map((item, key) =>key < 3 && (
+                    <HeaderItem Icon={item.icon} name={''}/>
+                
+                ))}
+            </div>
+            <div className='flex md:hidden gap-8' onClick={()=>setToggle(!toggle ?true : false)}>
+                    <HeaderItem Icon={HiDotsVertical} name={''} />
+                {toggle?<div className='absolute mt-5 bg-[#121212] border-[1px] border-gray-600 p-3 px-5 py-4'>
+                    {menu.map((item, key) =>key >= 3 && (
+                            <HeaderItem Icon={item.icon} name={item.name}/>
+                    ))}
+                </div>:null}
+                
+            </div>
+        </div>
+        <img src='https://wallpapers.com/images/hd/netflix-profile-pictures-5yup5hd2i60x7ew3.jpg' className='w-[40px] rounded-full' />
+    </div>
+  );
+}
+
+export default Header
