921af3c17afc70788548877f7e6203028f202f1b
Othman Salahi <132019758+OthmanSALAHI@users.noreply.github.com>
2024-02-05 12:56:52 +0100

    Update GlobalApi.jsx

--- src/services/GlobalApi.jsx
+++ src/services/GlobalApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
-const API_KEY = import.meta.env.API_KEY
+const API_KEY = process.env.API_KEY
 // the trending videos of movies 
 // https://api.themoviedb.org/3/trending/all/day?api_key=27ba81ba8ba427ba9bdac05eb30d2e26
 const TrendingMovies = axios.get(BASE_URL+'/trending/all/day?api_key='+API_KEY)
